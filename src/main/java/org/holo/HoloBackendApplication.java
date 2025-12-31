2f2cb6d327dd39ce6493ddb74e19d077845910b7
qiqd <xuyiji092854@126.com>
2025-12-31 19:37:41 +0800

    config(timezone): 设置默认时区为GMT+8
    - 在application.yml中配置Jackson时区为GMT+8
    - 在HoloBackendApplication中设置系统默认时区为GMT+8
    - 确保应用时间处理的一致性

--- src/main/java/org/holo/HoloBackendApplication.java
+++ src/main/java/org/holo/HoloBackendApplication.java
@@ -3,10 +3,13 @@ package org.holo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class HoloBackendApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
     SpringApplication.run(HoloBackendApplication.class, args);
   }
 
